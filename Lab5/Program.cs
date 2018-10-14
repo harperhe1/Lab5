d9a6e353a280c79052d1f90e0e38bac939d81f52
Heather <harperhe@hotmail.com>
2018-10-14 13:38:27 -0400

    inital commit

---
+++ Lab5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            bool cont = true;
+
+            do
+            {
+
+
+
+
+
+                Console.WriteLine("Welcome to the Factorial Calculator");
+                Console.WriteLine("Enter an integer that's greater than 0 but less than 10: ");
+                long input = long.Parse(Console.ReadLine());
+
+
+                long fact = input;
+
+                for (long i = input - 1; i >= 1; i--)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("The factorial of " + (input) + " is " + (fact));
+                Console.WriteLine("Continue?(y/n)");
+                bool cont2 = true;
+                while (cont2)
+                {
+                    string yesOrNo;
+
+                    yesOrNo = Console.ReadLine().ToLower();
+
+                    if (yesOrNo == "y")
+                    {
+                        cont2 = false;
+                    }
+
+                    else if (yesOrNo == "n")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        cont = false;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Try Again!");
+                    }
+
+                }
+
+            } while (cont);
+            
+        }
+    }
+}
